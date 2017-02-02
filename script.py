303d89c49957965bd0b49561b410c3749a9d4a81
Glukoosi <aleksi@seppanen.me>
2017-02-02 18:39:53 +0200

    added Message class

--- script.py
+++ script.py
@@ -1,18 +1,42 @@
 import socket
+import os
+import time
+import sys
+from time import strftime, localtime
+
 import colorama
 from colorama import Fore, Back, Style
+
 
 ircserver = "irc.elisa.fi"
 port = 6667
 
-nick = "kiltisbot"
-username = "kiltisbot"
-realname = "kiltisbot"
+nick = "kiltahuone"
+username = "kiltahuone"
+realname = "kiltahuone"
 channels = ["#kiltisbottest", "#kiltisbottest2"]
-	
-def main():
-	irc = connect()
-	inputloop(irc)
+
+
+class Message:
+	def __init__(self, data):
+		self.channel = ""
+		self.sender = ""
+		self.channel = ""
+
+		self.time = strftime("%H:%M", localtime())
+		data = data.split()
+		if data[0] == "PING":
+			irc.send ("PONG {}\r\n".format(data[1]).encode('utf-8'))
+		else:
+			self.sender = data[0].lstrip(":").split("!")[0]
+			if data[2] == nick:
+				self.channel = "query"
+			elif data[2] == ircserver:
+				self.channel = "msg form server"
+			else:
+				self.channel = data[2]
+			self.msg = " ".join(data[3:])[1:]
+
 
 def connect():
 	irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,30 +48,25 @@ def connect():
 		irc.send("JOIN {}\n".format(channel).encode('utf-8'))
 	return irc
 
-def inputloop(irc):
+def inputloop():
 	while True:
-		try:
-			data = str(irc.recv(4096),"UTF-8", "replace")
-			data = data.split()
-			name = data[0].lstrip(":").split("!")
-			msg = " ".join(data[3:])
-			msg = msg[1:]
-			if "PING" == data[0]:
-				irc.send ( "PONG {}\r\n".format(data[1]).encode('utf-8'))
-			for channel in channels:
-				if channel == data[2]:
-					channelm(irc, data[2], name[0], msg)
-			if "kiltisbot" == data[2] and name[0] != ircserver:
-				query(irc, name[0], msg)
-		except IndexError:
-			pass
+		m = Message(str(irc.recv(4096),"UTF-8", "replace"))
+		for channel in channels:
+			if channel == m.channel:
+				channelm(irc, m)
+		if m.channel == "query":
+			query(irc, m)
 			
-def query(irc, name, msg):
-	print( Fore.YELLOW + "query from " + Fore.RED + name + Style.RESET_ALL + ": " + msg)
+def query(irc, m):
+	print("{} {}query from {}{}: {}{}".format(m.time, Fore.YELLOW, Fore.RED, m.sender ,Style.RESET_ALL, m.msg))
 	return 0
 
-def channelm(irc, channel, name, msg):
-	print( Fore.YELLOW + channel + " " + Fore.RED + name + Style.RESET_ALL + ": " + msg)
+def channelm(irc, m):
+	print("{} {}{} {}{}: {}{}".format(m.time, Fore.YELLOW, m.channel, Fore.RED, m.sender ,Style.RESET_ALL, m.msg))
+
 	return 0
 
-main()
+if __name__ == "__main__":
+	os.system("clear")
+	irc = connect()
+	inputloop()
